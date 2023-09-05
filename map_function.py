0e6df8b3aee4596c7313c0ec8b9b42b6dbe4f31e
mkcearun89 <144054180+mkcearun89@users.noreply.github.com>
2023-09-05 16:45:11 +0530

    Create map_function.py

---
+++ map_function.py
@@ -0,0 +1,11 @@
+# Map function ---
+
+# map function is a transfarmation function which takes two arguments.
+
+## 1.the function 
+## 2.the items which are to be iteratable
+
+def mul(i):
+  return i*5
+x=map(mul,[3,5,7])
+print(list(x))
