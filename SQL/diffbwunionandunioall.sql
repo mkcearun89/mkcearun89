de08c5ed6e6b9afda26ac1453d796b506814f27a
mkcearun89 <144054180+mkcearun89@users.noreply.github.com>
2023-09-25 15:28:11 +0530

    Update diffbwunionandunioall.sql

--- SQL/diffbwunionandunioall.sql
+++ SQL/diffbwunionandunioall.sql
@@ -1,14 +1,14 @@
 -- difference between union and union all ---
 
 --- union used to combine the result of two select statements 
--- the result of union will have the duplicates
+-- the result of union will remove the duplicates
 --- union all is also used to combine the result of two select statements
---- union all result set will eliminate the duplicates and the result will be in the ascending order 
+--- union all result set will include the duplicates and the result will be in the ascending order 
 
 select dept_id from emp1
 union
 select dept_id from emp2
 
-select dept_id from emp1  -------- this will remove the duplicates and result will be in ascending order 
+select dept_id from emp1  -------- this will include the duplicates and result will be in ascending order 
 union all
 select dept_id from emp2
