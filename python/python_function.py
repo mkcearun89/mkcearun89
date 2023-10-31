2f0a1604a0eab50d3f3101e9e4d0da6ecd623d53
mkcearun89 <144054180+mkcearun89@users.noreply.github.com>
2023-10-31 12:43:32 +0530

    Update python_function.py

--- python/python_function.py
+++ python/python_function.py
@@ -6,3 +6,16 @@ def findsquare(in_num):
     return(in_num*in_num)
     
 print(findsquare(5))
+
+"""
+program to find polindrome string
+"""
+def ispolindrome(s):
+    rev=''.join(reversed(s))
+    if (s==rev):
+        return True
+    else:
+        return False
+        
+print (ispolindrome('malayalam'))
+        
