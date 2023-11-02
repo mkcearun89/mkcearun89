75bb631d9cfdf69a4dde6981c09feb0d000ad9f4
mkcearun89 <144054180+mkcearun89@users.noreply.github.com>
2023-11-02 11:13:33 +0530

    Update Nov_02_2023.py

--- python/Nov_02_2023.py
+++ python/Nov_02_2023.py
@@ -13,3 +13,21 @@ lt1=proper_divisor(220)
 print(lt1)
 
 #############################################################################################
+"""
+program to find leap years for the next 15 years of given given year
+
+def leapyear(input_year):
+    oplt=[]
+    for input_year in range(input_year,input_year+15):
+        if(input_year%400 == 0 and input_year%100== 0):
+            oplt.append(input_year)
+            input_year+=1
+        elif(input_year%4== 0 and input_year%100!=0):
+            oplt.append(input_year)
+            input_year+=1
+        else:
+            input_year+=1
+    return(oplt)
+
+print(leapyear(2010))
+######################################################################################################
