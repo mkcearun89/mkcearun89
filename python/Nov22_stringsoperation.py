8bff95d998a7a9757ce888c0e25010ece0623bb7
mkcearun89 <144054180+mkcearun89@users.noreply.github.com>
2023-11-22 22:55:26 +0530

    Update Nov22_stringsoperation.py

--- python/Nov22_stringsoperation.py
+++ python/Nov22_stringsoperation.py
@@ -33,3 +33,34 @@ print(lt[2:])
 
 str="Arunkumar"
 print(str[0:5])
+
+
+## to find length of a string
+str="Arunkumar"
+print('The length of a string - ', len(str))
+
+print('printing the string vertically')
+for i in str:	
+	print(i)
+    
+
+##strings are immutable
+str="Arunkumar"
+t=str[0]
+print('Example says strings are immutable - ', str[0])
+
+##
+str2="Chennai"
+print(str+str2[0:])
+
+print ("i" in str2)
+
+print("z" in str2)
+
+#join
+
+str1="Arun"
+print(str1.join(str2))
+
+str1="India is a great country"
+print(str1.split(" "))
