42946c4cd0352beb2905b07c9040c6ee1a15f42f
mkcearun89 <144054180+mkcearun89@users.noreply.github.com>
2023-11-22 22:03:59 +0530

    Create Nov22_stringsoperation.py

---
+++ python/Nov22_stringsoperation.py
@@ -0,0 +1,35 @@
+##printing a string
+str="Arunkumar"
+
+print('printing a string - ' , str)
+
+##printing a string in upper case
+print('the upper case example -', str.upper())
+
+##printing a string in lower case
+str="ARUNKUMAR"
+print('the lower case example -', str.lower())
+
+#capitalize - making first letter to upper case
+str='arunkumar'
+print(str.capitalize())
+
+#swapcase
+str='Arun Kumar'
+print(str.swapcase())
+
+#to get the first letter from the string
+str='Arunkumar'
+print(str[0])
+
+
+str='Arunkumar'
+print(str[2:])
+print(str[:2])
+
+lt=[10,2,3,4,5,6,67,8,9]
+print(lt[2:])
+
+
+str="Arunkumar"
+print(str[0:5])
