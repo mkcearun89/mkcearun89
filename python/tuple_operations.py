a984157c795eb2ded0252255addfacf7d296a11c
mkcearun89 <144054180+mkcearun89@users.noreply.github.com>
2023-11-23 23:24:53 +0530

    Create tuple_operations.py

---
+++ python/tuple_operations.py
@@ -0,0 +1,22 @@
+print('multiplying tuple')
+
+a=(3,4,5)
+print(a*3)
+
+print('printing using for loop')
+a=(4,5,6)
+for i in a:
+    print(i)
+    
+
+print('number of times the element appear in the tuple')
+print('count a element in a tuple')
+a=(5,67,78)
+print(a.count(5))
+
+print('finding the index of a element')
+print(a.index(67))
+
+print('to find the min and max of a tuple')
+print(min(a))
+print(max(a))
